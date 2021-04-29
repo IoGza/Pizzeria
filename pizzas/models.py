14e3e03cdb8b9fded90303332648ca0224e08e29
Evan McKinney-Larkin <evanmckinney07@gmail.com>
2021-04-28 23:40:45 -0500

    returned in str

--- pizzas/models.py
+++ pizzas/models.py
@@ -6,7 +6,11 @@ class Pizza(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.text
+        return self.name
 class Topping(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete = models.CASCADE)
     name = models.TextField()   
+
+    def __str__self(self):
+        return self.pizza
+        return self.name
